06bf116d66f099ed1f2c6b81e4db8c537491bd5a
Lu Daley <daleyluis@gmail.com>
2019-02-12 22:57:08 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,7 @@ def my_find(collection)
    i = 0
    while i < collection.length
 binding.pry
-  #   yield(collection[i])
-  #   i = i + 1
-  # end
+    yield(collection[i])
+    i = i + 1
+  end
 end
